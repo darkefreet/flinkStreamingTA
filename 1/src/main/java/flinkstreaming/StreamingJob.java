54bd6a2a72931df9c5648dec045913fee602680d
Wilhelmus Andrian <wilhelm_andrean_22@yahoo.com>
2017-04-12 11:38:33 +0700

    init

---
+++ 1/src/main/java/flinkstreaming/StreamingJob.java
@@ -0,0 +1,95 @@
+package flinkstreaming;
+
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import org.apache.flink.api.common.functions.FlatMapFunction;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.util.Collector;
+import org.apache.flink.streaming.connectors.twitter.TwitterSource;
+
+import java.util.Properties;
+
+
+/**
+ * Skeleton for a Flink Streaming Job.
+ *
+ * For a full example of a Flink Streaming Job, see the SocketTextStreamWordCount.java
+ * file in the same package/directory or have a look at the website.
+ *
+ * You can also generate a .jar file that you can submit on your Flink
+ * cluster.
+ * Just type
+ * 		mvn clean package
+ * in the projects root directory.
+ * You will find the jar in
+ * 		target/1-1.0-SNAPSHOT.jar
+ * From the CLI you can then run
+ * 		./bin/flink run -c flinkstreaming.StreamingJob target/1-1.0-SNAPSHOT.jar
+ *
+ * For more information on the CLI see:
+ *
+ * http://flink.apache.org/docs/latest/apis/cli.html
+ */
+public class StreamingJob {
+
+	public static void main(String[] args) throws Exception {
+
+		// set up the execution environment
+		final StreamExecutionEnvironment env = StreamExecutionEnvironment
+				.getExecutionEnvironment();
+
+		// get input data
+		Properties props = new Properties();
+		props.setProperty(TwitterSource.CONSUMER_KEY, "");
+		props.setProperty(TwitterSource.CONSUMER_SECRET, "");
+		props.setProperty(TwitterSource.TOKEN, "");
+		props.setProperty(TwitterSource.TOKEN_SECRET, "");
+		DataStream<String> text = env.addSource(new TwitterSource(""));
+
+		DataStream<Tuple2<String, Integer>> counts =
+				// split up the lines in pairs (2-tuples) containing: (word,1)
+				text.flatMap(new SocketTextStreamWordCount.LineSplitter())
+						// group by the tuple field "0" and sum up tuple field "1"
+						.keyBy(0)
+						.sum(1);
+
+		counts.print();
+
+		// execute program
+		env.execute("Java WordCount from SocketTextStream Example");
+	}
+
+	public static final class LineSplitter implements FlatMapFunction<String, Tuple2<String, Integer>> {
+
+		@Override
+		public void flatMap(String value, Collector<Tuple2<String, Integer>> out) {
+			// normalize and split the line
+			String[] tokens = value.toLowerCase().split("\\W+");
+
+			// emit the pairs
+			for (String token : tokens) {
+				if (token.length() > 0) {
+					out.collect(new Tuple2<String, Integer>(token, 1));
+				}
+			}
+		}
+	}
+}
